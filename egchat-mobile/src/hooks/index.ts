28fe313c87f484eb6a05a9112ff2f4df8ef234b7
patronbernardo111-stack <patron.bernardo111@gmail.com>
2026-05-05 16:11:03 +0100

    deploy: auto build and push

---
+++ egchat-mobile/src/hooks/index.ts
@@ -0,0 +1,6 @@
+// Hooks barrel export
+export { useAuth } from './useAuth';
+export { useOffline } from './useOffline';
+export { useGPS, distanceKm, formatCoords } from './useGPS';
+export { useDevice } from './useDevice';
+export * from './useSounds';
